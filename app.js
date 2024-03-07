a0542fd238808b7f5ed2ebc1f9de461ae5514645
lkn2512 <lkn25122001@gmail.com>
2024-03-07 20:56:00 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 // define error-handling middleware last, after other app.use() and routes calls
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
     // Middleware xử lý lỗi tập trung.
     // Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
-    return res.status(Error.statusCode || 500).json({
-    message: Error.message || "Internal Server Error",
+    return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
     });
 });
 
